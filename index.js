72660f33596ee8b22cdcd72e62f3c1bce6c68d7b
robertoandres24 <robertoandres24@gmail.com>
2023-06-10 19:43:28 -0400

    map questions and save into a json file

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
-  const width = 1920;
-  const height = 1080;
-  const browser = await puppeteer.launch({ headless: false, defaultViewport: { width, height } });
+
+  function checkOptions(options, orderAnswer, optionCorrect) {
+    const correctIndexes = optionCorrect.split(',').map(Number).map(i => i - 1);
+    const expectedOrder = orderAnswer.split('').map(Number).map(i => i - 1);
+    const selectedOptions = expectedOrder.map(i => options[i]);
+    const selectedIndexes = [];
+    for (let i = 0; i < correctIndexes.length; i++) {
+      const correctIndex = correctIndexes[i];
+      const selectedIndex = expectedOrder.indexOf(correctIndex);
+      if (selectedIndex >= 0 && selectedIndexes.indexOf(selectedIndex) < 0) {
+        selectedIndexes.push(selectedIndex);
+      }
+    }
+    return selectedIndexes;
+  }
+
+  const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized'] });
   const page = await browser.newPage();
-  await page.setViewport({ width, height });
+  await page.setUserAgent('Mi-User-Agent-String-Aquí');
+  await page.exposeFunction('checkOptions', checkOptions);
+  await page.setViewport({ width: 1920, height: 1080 });
 
   await page.setUserAgent('UA-TEST');
   await page.goto('https://practicatest.cl/examen-teorico/clase-B');
@@ -27,29 +44,41 @@ const puppeteer = require('puppeteer');
   });
 
   const contentTest = await page.waitForSelector('#content-test')
-  console.log("🚀 ~ file: index.js:32 ~ contentTest:", contentTest)
+
   await contentTest.scrollIntoView()
   await new Promise(r => setTimeout(r, 3000));
 
-  await contentTest.evaluate(node => {
+  const mappedQuestions = await contentTest.evaluate(node => {
     const boxes = node.querySelectorAll('.well');
-    const result = [...boxes].map(box => {
+    const mappedQuestionPromises = [...boxes].map(async box => {
       const question = box.querySelector('.col-md-8 h4').textContent;
       const options = [...box.querySelectorAll('.col-md-8 .checklist li')].map(li => li.textContent);
       const optionCorrect = box.querySelector('.option_correct').textContent;
       const orderAnswer = box.querySelector('.order_answer').textContent;
-      console.log("🚀 ~ file: index.js:43 ~ result ~ orderAnswer:", orderAnswer)
-      console.log("🚀 ~ file: index.js:42 ~ result ~ optionCorrect:", optionCorrect)
-      console.log("🚀 ~ file: index.js:41 ~ result ~ options:", JSON.stringify(options))
-      console.log("🚀 ~ file: index.js:40 ~ result ~ question:", question)
-      return { question, options, optionCorrect, orderAnswer }
+      const correctAnswers = await checkOptions(options, orderAnswer, optionCorrect);
+      return { question, options, correctAnswers };
+
     });
-    console.log("🚀 ~ file: index.js:39 ~ result:", result)
-
+    return Promise.all(mappedQuestionPromises);
   })
+  // console.log("🚀 ~ file: index.js:66 ~ mappedQuestions ~ mappedQuestions:", JSON.stringify(mappedQuestions))
+  // write the mapped questions to a file
+  console.log('writing to file...');
+  // write file with current date and time
 
-  // puppeteer screenshot
-  await page.screenshot({ path: './example.png' });
+  const now = new Date();
+  const filename = `file_${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}_${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}.json`;
 
   await browser.close();
+
+  fs.writeFile(filename, JSON.stringify(mappedQuestions), 'utf8', (err) => {
+    if (err) {
+      console.log(`Error writing file: ${err}`);
+    } else {
+      console.log('File saved');
+      console.log('closing file...');
+      fs.closeSync(fs.openSync(filename, 'r'));
+      console.log('File closed');
+    }
+  });
 })();
