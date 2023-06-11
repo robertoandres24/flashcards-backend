b31f1b60625393a8c4364a718d7cc5887b48aac7
robertoandres24 <robertoandres24@gmail.com>
2023-06-11 01:04:51 -0400

    refactor saveUser function

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,25 @@
-import mongoose, { ConnectOptions } from 'mongoose';
+import mongoose, { ConnectOptions, Schema } from 'mongoose';
 
 // Define the schema for your collection
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   name: String,
   age: Number
 });
 
 // Create a model based on the schema
 const User = mongoose.model('users', UserSchema);
+
+type UserType = {
+  name: string;
+  age: number;
+}
+
+const saveUser = async (user: UserType) => {
+  const userI = new User(user);
+  // save user
+  return userI.save()
+}
+
 async function connectToDB(): Promise<void> {
   try {
     await mongoose.connect('mongodb://localhost:27017/mydatabase', {
@@ -20,4 +32,4 @@ async function connectToDB(): Promise<void> {
   }
 }
 
-export { User, connectToDB, mongoose };
+export { connectToDB, mongoose, saveUser, UserType };
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-import { User, connectToDB, mongoose } from './db';
+import { connectToDB, saveUser, UserType, mongoose } from './db';
 
 // Llamar a la función para conectarse a MongoDB
 connectToDB();
 
-// Create a document to save
-const user = new User({ name: 'obando', age: 38 });
-// Save the document to the collection
+async function main() {
+  // Create a document to save
+  const user: UserType = {
+    name: 'paul',
+    age: 30
+  }
+  const userSaved = await saveUser(user);
+  console.log("🚀 ~ file: index.ts:13 ~ main ~ userSaved:", userSaved)
+  mongoose.disconnect();
+}
+try {
+  main();
+} catch (error) {
+  console.error('Error saving document:', error);
+  mongoose.disconnect();
+}
 
-user.save()
-  .then(savedUser => {
-    console.log('Document saved:', savedUser);
-    mongoose.disconnect();
-    // close connectio
-  })
-  .catch(error => {
-    console.error('Error saving document:', error);
-    mongoose.disconnect();
-  });
